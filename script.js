7187c22534da33806bfe87f6f69275bcb2694766
MEDCAREBOT <112470205+nguyenluutridung@users.noreply.github.com>
2026-01-22 23:00:47 +0700

    hm

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const searchInput = document.getElementById('searchInput');
 const searchBtn = document.getElementById('searchBtn');
-
 function handleSearch() {
     const query = searchInput.value.trim();
     if (query) {
@@ -9,7 +8,6 @@ function handleSearch() {
         searchInput.value = "";
     }
 }
-
 searchBtn.addEventListener('click', handleSearch);
 searchInput.addEventListener('keypress', function(e) {
     if (e.key === 'Enter') handleSearch();
